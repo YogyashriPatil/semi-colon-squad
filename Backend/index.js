32d390af9eb89870cf82f1c52c79f454a978e073
YogyashriPatil <yogyashripatil20@gmail.com>
2026-02-28 11:08:22 +0530

    Initlize the auth route

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js"
+import projectRoutes from "./routes/projectRoutes.js";
+import drawingRoutes from "./routes/drawingRoutes.js";
+import estimationRoutes from "./routes/estimationRoutes.js";
+import timelineRoutes from "./routes/timelineRoutes.js";
+import reportRoutes from "./routes/reportRoutes.js";
+import rateRoutes from "./routes/rateRoutes.js";
+dotenv.config();
+connectDB(); 
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+// console.log(projectRoutes);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
